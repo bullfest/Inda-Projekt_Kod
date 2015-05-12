6f922a13ada62d5364ff15283573809e72e3445e
Alexander Viklund <viklu@kth.se>
2015-05-12 13:21:57 +0200

    Enable ship steering, fixes some bugs around keypresses

--- src/Pirates.java
+++ src/Pirates.java
@@ -1,5 +1,6 @@
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Alexander on 2015-05-08.
@@ -10,9 +11,9 @@ public class Pirates {
 
     public static void main(String[] args) throws InterruptedException{
         Window window = new Window(1700, 1000, new KeyListener());
-
-        Ship ship1 = new Ship(new Point(50,50),pressedKeys,new int[] {});
-        Ship ship2 = new Ship(new Point(100,100),pressedKeys,new int[] {});
+        Integer[] keysShip1 = new Integer[] {87,83,65,68,(int)'v'};
+        Ship ship1 = new Ship(new Point(50,50),pressedKeys,Arrays.asList(keysShip1));
+        Ship ship2 = new Ship(new Point(100,100),pressedKeys,Arrays.asList(keysShip1));
 
 		long timeDiff;
 		long lastTime = System.currentTimeMillis(); // System.currentTimeMillis() returns a long
@@ -21,13 +22,12 @@ public class Pirates {
         ship1.setAngle(15);
         ship2.setAngle(90);
         window.add(ship2);
-        for (int i = 0; i < 360; i++) {
+        while (true){
 			// First loop iteration might be strange if the initial value of lastTime is wrong
 			timeDiff = System.currentTimeMillis() - lastTime;
 			lastTime = System.currentTimeMillis();
-            ship1.setAngle(i);
-            ship2.update(timeDiff);
-            ship2.setAngle(i);
+            ship1.update((int)timeDiff);
+            ship2.update((int)timeDiff); //each loop shouldn't take more time than an int can hold
             window.repaint();
             Thread.sleep(10);
         }
@@ -42,7 +42,8 @@ public class Pirates {
 
         @Override
         public void keyPressed(KeyEvent e) {
-            pressedKeys.add(e.getKeyCode());
+            if (!pressedKeys.contains(e.getKeyCode()))
+                pressedKeys.add(e.getKeyCode());
         }
 
         @Override
--- src/Ship.java
+++ src/Ship.java
@@ -1,4 +1,5 @@
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Alexander on 2015-05-08.
@@ -6,27 +7,53 @@ import java.util.Collection;
 public class Ship extends Entity {
 
     private Point velocity = new Point(0,0);
-    private final int MAX_SPEED = 5;
-    private Collection<Integer> pressedKeys;
+    private ArrayList<Integer> pressedKeys;
 
-    public Ship(Point p, Collection<Integer> pressedKeys,int[] keys){
+    private final int MAX_SPEED = 4;
+    private final double ACCELERATION_PER_SECOND = 1.5, ROTATION_PER_SECOND = 60/*Degrees*/;
+
+    // {Up,Down,left,right,shoot}
+    List<Integer> keys;
+
+    public Ship(Point p, ArrayList<Integer> pressedKeys,List<Integer> keys){
         super(p, "resources/pictures/ship.png");
         this.pressedKeys = pressedKeys;
+        this.keys = keys;
     }
     
-    public void update(double timeDiff) {
-        Point acceleration = new Point(0,-1);
+    public void update(int timeDiff) {
+        velocity.multiply(1-0.5*(timeDiff/1000.0));
+        Point acceleration = new Point(0,0);
+
+        for(int i = 0; i < pressedKeys.size();i++) {
+            int index = keys.indexOf(pressedKeys.get(i));
+            if (index == 0) // Up
+                acceleration.add(new Point(0,-ACCELERATION_PER_SECOND));
+            if (index == 1) // Down
+                acceleration.add(new Point(0,ACCELERATION_PER_SECOND));
+            if (index == 2) // Left
+                setAngle(getAngle()+ROTATION_PER_SECOND*timeDiff/1000.0);
+            if (index == 3) // Right
+                setAngle(getAngle()-ROTATION_PER_SECOND*timeDiff/1000.0);
+            if (index == 4) // Shoot
+                shoot(timeDiff);
+        }
         acceleration.rotate(getAngle());
-        velocity.add(acceleration,timeDiff/500);
+        velocity.add(acceleration,timeDiff/1000.0);
 
         if (velocity.norm()>MAX_SPEED) {
             velocity.normalize();
             velocity.setData(velocity.multiply(MAX_SPEED).getData());
         }
-
-        getPos().add(velocity);
+        Point center = getCenterPos();
+        center.add(velocity);
+        setCenter(center);
 
 	}
+
+    private void shoot(int timeDiff) {
+
+    }
 
     public boolean isColliding(Entity e) {
         //ToDo: Implement
