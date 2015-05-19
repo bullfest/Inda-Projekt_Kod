a179325a58ea8f723e0ac88a8be05f794133ad03
Alexander Viklund <viklu@kth.se>
2015-05-19 16:34:48 +0200

    Clean up main method

--- src/Pirates.java
+++ src/Pirates.java
@@ -14,15 +14,19 @@ public class Pirates {
     public static final int WINDOW_HEIGHT = 1000, WINDOW_WIDTH = 1700;
 
     public static void main(String[] args) throws InterruptedException{
+
         window = new Window(WINDOW_WIDTH, WINDOW_HEIGHT, new KeyListener());
+
+        //Create ships
         Integer[] keysShip1 = new Integer[] {87,83,65,68,86};
         Integer[] keysShip2 = new Integer[] {38,40,37,39,96};
-        Ship ship1 = new Ship(new Point(50,50),pressedKeys,Arrays.asList(keysShip1));
-        Ship ship2 = new Ship(new Point(100,100),pressedKeys,Arrays.asList(keysShip2));
+        ArrayList<Ship> ships = new ArrayList<Ship>();
+        ships.add(new Ship(new Point(50,50),pressedKeys,Arrays.asList(keysShip1)));
+        ships.add(new Ship(new Point(100,100),pressedKeys,Arrays.asList(keysShip2)));
+        window.add(ships.get(0));
+        window.add(ships.get(1));
 
-		long timeDiff;
-		long lastTime = System.currentTimeMillis(); // System.currentTimeMillis() returns a long
-
+        //Create Islands
         Random random = new Random();
         ArrayList<Island> islands = new ArrayList<Island>();
         int numberOfIslands = random.nextInt(10);
@@ -31,25 +35,25 @@ public class Pirates {
             window.add(islands.get(i));
         }
 
-        window.add(ship1);
-        window.add(ship2);
+        long timeDiff;
+        long lastTime = System.currentTimeMillis(); // System.currentTimeMillis() returns a long
 
         while (true){
 			// First loop iteration might be strange if the initial value of lastTime is wrong
 			timeDiff = System.currentTimeMillis() - lastTime;
 			lastTime = System.currentTimeMillis();
-            ship1.update((int)timeDiff);
-            ship2.update((int)timeDiff); //each loop shouldn't take more time than an int can hold
+            for (Ship s:ships) {
+                s.update((int)timeDiff); //each loop shouldn't take more time than an int can hold
+            }
 
             ArrayList<Cannonball> cannonballsClone = (ArrayList<Cannonball>)cannonballs.clone();
             for(Cannonball c:cannonballsClone) {
 				c.update((int)timeDiff);
 
                 //Do cannonball collisions
-                if (ship1.isColliding(c))
-                    c.collideWith(ship1);
-                if (ship2.isColliding(c))
-                    c.collideWith(ship2);
+                for (Ship s:ships)
+                    if (s.isColliding(c))
+                        c.collideWith(s);
                 for (Island island:islands) {
                     if (c.isColliding(island))
                         island.collideWith(c);
@@ -59,17 +63,19 @@ public class Pirates {
             //Do other collisions
 
             for (Island island:islands) {
-                if (ship1.isColliding(island))
-                    island.collideWith(ship1);
-                if (ship2.isColliding(island))
-                    island.collideWith(ship2);
+                for (Ship s:ships)
+                    if (s.isColliding(island))
+                        island.collideWith(s);
             }
 
-            if (ship1.isColliding(ship2)) {
-                ship2.collideWith(ship1);
-                ship1.collideWith(ship2);
+            for (int i = 0; i < ships.size()-1; i++) {
+                for (int j = i+1;j<ships.size();j++) {
+                    if (ships.get(i).isColliding(ships.get(j))) {
+                        ships.get(i).collideWith(ships.get(j));
+                        ships.get(j).collideWith(ships.get(i));
+                    }
+                }
             }
-
 
             window.repaint();
             Thread.sleep(10);
